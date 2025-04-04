386d420c7bc5f38a040f2f33151a970b655067f5
Victor Martin Arquelladas <vmn@stiga.es>
2025-04-04 14:26:05 +0200

    Página de héroe

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
@@ -8,11 +11,30 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class HeroPageComponent implements OnInit{
 
-  constructor(private HeroesService: HeroesService){
+  public hero?: Hero;
 
-  }
+  constructor(
+    private HeroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ){  }
   ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({ id }) => this.HeroesService.getHeroById( id )),
+    )
+    .subscribe( Hero =>{
 
+      if(!Hero) return this.router.navigate(['/heroes/list']);
+
+      this.hero = Hero;
+      console.log({Hero})
+      return;
+    } )
+  }
+
+  goBack(): void{
+    this.router.navigateByUrl('heroes/list')
   }
 
 
